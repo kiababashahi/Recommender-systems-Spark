758d11f12913844183c3eac4339e58c374db9614
kiababashahi <kiababashahi@gmail.com>
2018-03-02 18:48:11 -0500

    the 3rd is complete and the 4th has errors

--- answers/basic_als_recommender.py
+++ answers/basic_als_recommender.py
@@ -3,7 +3,7 @@ from pyspark.ml.recommendation import ALS
 from pyspark.sql import Row, SparkSession
 import sys
 rand_seed=sys.argv[1]
-file="./data/sample_movielens_ratings.txt"
+file="../data/sample_movielens_ratings.txt"
 spark=SparkSession.builder.master("local").appName("LAB2").getOrCreate()
 lines=spark.read.text(file).rdd
 parts = lines.map(lambda row: row.value.split("::"))
